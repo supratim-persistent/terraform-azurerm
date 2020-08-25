a3658aa02bf6df8d5aafe78fe68da18545163858
Steve Dillon <stvdilln@yaponcha.com>
2020-08-25 09:44:35 -0400

    Missed this change

--- services/keyvault/endpoint/base/v1.1/monitor.tf
+++ services/keyvault/endpoint/base/v1.1/monitor.tf
@@ -10,6 +10,7 @@ resource "azurerm_monitor_diagnostic_setting" "keyvault_diagnostic_setting" {
 
     retention_policy {
       enabled = true
+      days = var.monitor_settings.retention_days
     }
   }
 
@@ -18,12 +19,13 @@ resource "azurerm_monitor_diagnostic_setting" "keyvault_diagnostic_setting" {
 
     retention_policy {
       enabled = true
+      days = var.monitor_settings.retention_days
     }
   }
 }
 resource "azurerm_monitor_diagnostic_setting" "keyvault_log_setting" {
 
-  name                        = "${var.service_settings.name}-keyvault-logs"
+  name                        = "${var.service_settings.name}-keyvault-storage"
   target_resource_id          = azurerm_key_vault.keyvault.id
   storage_account_id          = var.monitor_settings.workspace_id
 
@@ -33,6 +35,7 @@ resource "azurerm_monitor_diagnostic_setting" "keyvault_log_setting" {
 
     retention_policy {
       enabled = true
+      # 0 means forever
       days = 0
     }
   }
