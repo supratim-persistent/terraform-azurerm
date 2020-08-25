2d88a56d7f033822554c5f6fb8d81e712313e4e7
Steve Dillon <stvdilln@yaponcha.com>
2020-08-25 09:54:20 -0400

    rename log settings to observability_settings

--- services/keyvault/endpoint/base/v1.1/monitor.tf
+++ services/keyvault/endpoint/base/v1.1/monitor.tf
@@ -2,7 +2,7 @@ resource "azurerm_monitor_diagnostic_setting" "keyvault_diagnostic_setting" {
 
   name                        = "${var.service_settings.name}-keyvault-logs"
   target_resource_id          = azurerm_key_vault.keyvault.id
-  log_analytics_workspace_id  = var.monitor_settings.workspace_id
+  log_analytics_workspace_id  = var.observability_settings.workspace_id
 
   log {
     category = "AuditEvent"
@@ -10,7 +10,7 @@ resource "azurerm_monitor_diagnostic_setting" "keyvault_diagnostic_setting" {
 
     retention_policy {
       enabled = true
-      days = var.monitor_settings.retention_days
+      days = var.observability_settings.retention_days
     }
   }
 
@@ -19,7 +19,7 @@ resource "azurerm_monitor_diagnostic_setting" "keyvault_diagnostic_setting" {
 
     retention_policy {
       enabled = true
-      days = var.monitor_settings.retention_days
+      days = var.observability_settings.retention_days
     }
   }
 }
@@ -27,7 +27,7 @@ resource "azurerm_monitor_diagnostic_setting" "keyvault_log_setting" {
 
   name                        = "${var.service_settings.name}-keyvault-storage"
   target_resource_id          = azurerm_key_vault.keyvault.id
-  storage_account_id          = var.monitor_settings.workspace_id
+  storage_account_id          = var.observability_settings.workspace_id
 
   log {
     category = "AuditEvent"
