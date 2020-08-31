2072f05cee16dcb70dc3a57908a3f8d6e8696b3d
supratim-persistent <68080807+supratim-persistent@users.noreply.github.com>
2020-08-31 16:10:01 -0400

    Update variables.tf

--- scenarios/microservices/fn/http/premium/base/v1.2/variables.tf
+++ scenarios/microservices/fn/http/premium/base/v1.2/variables.tf
@@ -45,3 +45,9 @@ variable "observability_settings" {
     workspace_id              = string
   })
 }
+variable "ip_rules_settings" {
+    type = list(object({
+        ip_address    = string
+        subnet_mask = string
+    }))
+}
